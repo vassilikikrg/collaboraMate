f31302b55b44d3dc77b3b710bddd5c986d9be041
Jimsidi <jimsid@hotmail.gr>
2024-01-11 18:32:35 +0200

    login form inside a bootstrap container and changed login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
