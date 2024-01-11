9e0d933ed6e65e737864708f633e23878fdb1528
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2024-01-11 18:44:32 +0200

    Add column 'name' to users table and modify signout action method to include name

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
--- db/migrate/20240111161119_devise_create_users.rb
+++ db/migrate/20240111161119_devise_create_users.rb
@@ -6,7 +6,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :name, null: false, default: ""
+      
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
