c7abf3a9786cb90b278b944d46780f19fd80ecac
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2024-01-12 16:46:50 +0200

    Database seeding using Faker to generate    User, Category and Post records inside the development database

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,3 @@ class Post < ApplicationRecord
     belongs_to :user
     belongs_to :category
   end
-  class User < ApplicationRecord
-    ...
-    has_many :posts, dependent: :destroy       
-  end
-  
-  class Category < ApplicationRecord
-    has_many :posts
-  end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,52 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+def seed_users
+    user_id = 0
+    10.times do 
+      User.create(
+        name: "test#{user_id}",
+        email: "test#{user_id}@test.com",
+        password: '123456',
+        password_confirmation: '123456'
+      )
+      user_id = user_id + 1
+    end
+  end
+  
+  
+  def seed_categories
+    hobby = ['Arts', 'Crafts', 'Sports', 'Sciences', 'Collecting', 'Reading', 'Other']
+    study = ['Arts and Humanities', 'Physical Science and Engineering', 'Math and Logic',
+            'Computer Science', 'Data Science', 'Economics and Finance', 'Business',
+            'Social Sciences', 'Language', 'Other']
+    team = ['Study', 'Development', 'Arts and Hobby', 'Other']
+  
+    hobby.each do |name|
+      Category.create(branch: 'hobby', name: name)
+    end
+  
+    study.each do |name|
+      Category.create(branch: 'study', name: name)
+    end
+  
+    team.each do |name|
+      Category.create(branch: 'team', name: name)
+    end
+  end
+  
+  def seed_posts
+    categories = Category.all
+  
+    categories.each do |category|
+      5.times do
+        Post.create(
+          title: Faker::Lorem.sentences[0], 
+          content: Faker::Lorem.sentences[0], 
+          user_id: rand(1..9), 
+          category_id: category.id
+        )
+      end
+    end
+  end
+  
+  seed_users
+  seed_categories
+  seed_posts
