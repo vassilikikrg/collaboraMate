eac2d7f6499e1e9ba55c761d3b18af481eaaf14e
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2024-01-12 16:33:24 +0200

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
-end
+    belongs_to :user
+    belongs_to :category
+  end
+  class User < ApplicationRecord
+    ...
+    has_many :posts, dependent: :destroy       
+  end
+  
+  class Category < ApplicationRecord
+    has_many :posts
+  end
