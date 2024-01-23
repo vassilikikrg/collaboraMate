4c3103ae39763126845c7fad0f258e46ea9b4daa
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2024-01-23 12:42:16 +0200

    Create a UI to create new posts
    For #22
    - Inside the PostsController:
      define new and create actions
      define a post_params method
      define a before_action filter
    - Inside the ApplicationController:
      define a redirect_if_not_signed_in method
      define a redirect_if_signed_in method
    - Create a new template for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
+  # do not allow not signed in users to have an access to create post page
+  before_action :redirect_if_not_signed_in, only: [:new] 
     def show
         @post = Post.find(params[:id])
-      end
+    end
       def hobby
         posts_for_branch(params[:action])
       end
@@ -14,7 +16,26 @@ class PostsController < ApplicationController
         posts_for_branch(params[:action])
       end 
 
+      def new
+        @branch = params[:branch]
+        @categories = Category.where(branch: @branch)
+        @post = Post.new
+      end
+    
+      def create
+        @post = Post.new(post_params)
+        if @post.save 
+          redirect_to post_path(@post) 
+        else
+          redirect_to root_path
+        end
+      end
+
   private
+  def post_params
+    params.require(:post).permit(:content, :title, :category_id)
+                         .merge(user_id: current_user.id)
+  end
   def posts_for_branch(branch)
     @categories = Category.where(branch: branch)
     @posts = get_posts.paginate(page: params[:page])
