469aea5f79c1284d8bb2b450479218b42b349321
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2024-01-23 11:34:39 +0200

    Modify get_posts method  to retrieve posts that meet the specified criteria in the filters
    - Define a default_scope for posts
    - Add a spec for the Post model's default_scope
    - Add by_category, by_branch and search scopes in the Post model
    - Modify the get_posts method in PostsController
    
    For #19

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,18 @@
 class Post < ApplicationRecord
     belongs_to :user
     belongs_to :category
+    default_scope -> { includes(:user).order(created_at: :desc) }
+
+    # Retrieves posts by a given branch and category_name by performing a join with the Category model.
+    scope :by_category, -> (branch, category_name) do 
+      joins(:category).where(categories: {name: category_name, branch: branch}) 
+    end
+    # Retrieves posts by a given branch by joining with the Category model.
+    scope :by_branch, -> (branch) do
+      joins(:category).where(categories: {branch: branch}) 
+    end
+    # Performs a case-insensitive search in the title and content columns based on a provided search term.
+    scope :search, -> (search) do
+      where("title ILIKE lower(?) OR content ILIKE lower(?)", "%#{search}%", "%#{search}%")
+    end
   end
