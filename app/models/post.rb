583659ef6422b7179aab71b7ccb58725c25b2e83
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2024-01-23 12:51:42 +0200

    Add validations to the Post model and specs to check them
    For #22

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ApplicationRecord
+    # Associations
     belongs_to :user
     belongs_to :category
     default_scope -> { includes(:user).order(created_at: :desc) }
 
+    # Scopes
     # Retrieves posts by a given branch and category_name by performing a join with the Category model.
     scope :by_category, -> (branch, category_name) do 
       joins(:category).where(categories: {name: category_name, branch: branch}) 
@@ -15,4 +17,9 @@ class Post < ApplicationRecord
     scope :search, -> (search) do
       where("title ILIKE lower(?) OR content ILIKE lower(?)", "%#{search}%", "%#{search}%")
     end
+
+    # Validations
+    validates :title, presence: true, length: { minimum: 5, maximum: 255 }
+    validates :content, presence: true, length: { minimum: 20, maximum: 1000 }
+    validates :category_id, presence: true
   end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -46,4 +46,52 @@ RSpec.describe Post, type: :model do
     expect(Post.search('great').count).to eq 1
     expect(Post.search('great')[0].id).to eq post.id
   end
+
+  context 'Validations' do
+    let(:post) { build(:post) }
+  
+    it 'creates succesfully' do 
+      expect(post).to be_valid
+    end
+  
+    it 'is not valid without a category' do 
+      post.category_id = nil
+      expect(post).not_to be_valid
+    end
+  
+    it 'is not valid without a title' do 
+      post.title = nil
+      expect(post).not_to be_valid
+    end
+  
+    it 'is not valid  without a user_id' do
+      post.user_id = nil
+      expect(post).not_to be_valid
+    end
+  
+    it 'is not valid  with a title, shorter than 5 characters' do 
+      post.title = 'a' * 4
+      expect(post).not_to be_valid
+    end
+  
+    it 'is not valid  with a title, longer than 255 characters' do 
+      post.title = 'a' * 260
+      expect(post).not_to be_valid
+    end
+  
+    it 'is not valid without a content' do 
+      post.content = nil
+      expect(post).not_to be_valid
+    end
+  
+    it 'is not valid  with a content, shorter than 20 characters' do 
+      post.content = 'a' * 10
+      expect(post).not_to be_valid
+    end
+  
+    it 'is not valid  with a content, longer than 1000 characters' do 
+      post.content = 'a' * 1050
+      expect(post).not_to be_valid
+    end
+  end 
 end
