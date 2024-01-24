a2d77d6cbb363b73a081d4cef8c7eefd2728d153
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2024-01-24 12:35:55 +0200

    Fix jquery not working
    For #23
    There is a jquery example in application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-//= require jquery
-//= require bootstrap-sprockets
+
+import "jquery"
+import "jquery_ujs"
+
+import "popper"
+import "bootstrap"
+
+
+$(document).ready(function(){
+    alert($("#test").attr('value'));
+   });
