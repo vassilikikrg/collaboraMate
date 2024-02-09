53527225b1124b34dbe12420a6737e87b3ed9be0
EktorasFokas <ectoras5555@gmail.com>
2024-02-09 12:16:12 +0200

    Add associations and helper methods to the User model
    for #15
    - Create relationship between the User the Contact models
    - Methods help query the Contact records

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,5 +8,77 @@ RSpec.describe User, type: :model do
       expect(association.macro).to eq :has_many
       expect(association.options[:dependent]).to eq :destroy
     end
+    it 'has_many contacts' do
+      association = described_class.reflect_on_association(:contacts)
+      expect(association.macro).to eq :has_many
+    end
+  
+    it 'has_many all_received_contact_requests' do
+      association = described_class.reflect_on_association(:all_received_contact_requests)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:class_name]).to eq 'Contact'
+      expect(association.options[:foreign_key]).to eq 'contact_id'
+    end
+  
+    it 'has_many accepted_sent_contact_requests' do
+      association = described_class.reflect_on_association(:accepted_sent_contact_requests)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:through]).to eq :contacts
+      expect(association.options[:source]).to eq :contact
+    end
+  
+    it 'has_many accepted_received_contact_requests' do
+      association = described_class.reflect_on_association(:accepted_received_contact_requests)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:through]).to eq :all_received_contact_requests
+      expect(association.options[:source]).to eq :user
+    end
+  
+    it 'has_many pending_sent_contact_requests' do
+      association = described_class.reflect_on_association(:pending_sent_contact_requests)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:through]).to eq :contacts
+      expect(association.options[:source]).to eq :contact
+    end
+  
+    it 'has_many pending_received_contact_requests' do
+      association = described_class.reflect_on_association(:pending_received_contact_requests)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:through]).to eq :all_received_contact_requests
+      expect(association.options[:source]).to eq :user
+    end
+  end
+
+  context 'Methods' do
+    let(:user) { build(:user) }
+    let(:contact_requests) do
+      @user = create(:user)
+      create_list(:contact, 2)
+      create_list(:contact, 2, accepted: true)
+      create(:contact, user_id: @user.id)
+      create(:contact, user_id: @user.id, accepted: true)
+      create(:contact, contact_id: @user.id)
+      create(:contact, contact_id: @user.id, accepted: true)
+    end
+  
+    it 'accepted_sent_contact_requests gets only accepted requests' do
+      contact_requests
+      expect(@user.accepted_sent_contact_requests.count).to eq 1
+    end
+  
+    it 'accepted_received_contact_requests gets only accepted requests' do
+      contact_requests
+      expect(@user.accepted_received_contact_requests.count).to eq 1
+    end
+  
+    it 'pending_sent_contact_requests gets only unaccepted requests' do
+      contact_requests
+      expect(@user.pending_sent_contact_requests.count).to eq 1
+    end
+  
+    it 'pending_received_contact_requests gets only unaccepted requests' do
+      contact_requests
+      expect(@user.pending_received_contact_requests.count).to eq 1
+    end
   end
 end
