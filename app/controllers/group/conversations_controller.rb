cdb2ae5430ca394dd589d8c47cb2d046ac81e10d
EktorasFokas <ectoras5555@gmail.com>
2024-02-11 21:32:58 +0200

    Add a close group conversation window functionality
    for #13
    - Define a close action inside the Group::ConversationsController
    - Create a close.js.erb inside the group/conversations

--- app/controllers/group/conversations_controller.rb
+++ app/controllers/group/conversations_controller.rb
@@ -36,4 +36,14 @@ class Group::ConversationsController < ApplicationController
       end
     end
     
+    def close
+      @conversation = Group::Conversation.find(params[:id])
+    
+      session[:group_conversations].delete(@conversation.id)
+    
+      respond_to do |format|
+        format.js
+      end
+    end
+    
   end
