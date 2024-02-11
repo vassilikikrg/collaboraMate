82285b3cf83d970123867ea1b2f20634970e952c
EktorasFokas <ectoras5555@gmail.com>
2024-02-11 21:45:51 +0200

    Create a Group::AddUserToConversationService and test it
    for #13

--- app/controllers/group/conversations_controller.rb
+++ app/controllers/group/conversations_controller.rb
@@ -46,4 +46,12 @@ class Group::ConversationsController < ApplicationController
       end
     end
     
+    def update
+      Group::AddUserToConversationService.new({
+        conversation_id: params[:id],
+        new_user_id: params[:user][:id],
+        added_by_id: params[:added_by]
+      }).call
+    end
+    
   end
