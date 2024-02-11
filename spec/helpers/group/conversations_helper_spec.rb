e8c4396712166cda63bbf200a588b62d50185a52
EktorasFokas <ectoras5555@gmail.com>
2024-02-11 21:13:52 +0200

    Create _select_user.html.erb and _messages_list.html.erb inside   group/conversations/conversation
    for #13
    - Define a load_group_messages_partial_path helper method
      and write specs for it

--- spec/helpers/group/conversations_helper_spec.rb
+++ spec/helpers/group/conversations_helper_spec.rb
@@ -21,4 +21,21 @@ context '#add_people_to_group_conv_list' do
     helper.stub(:current_user).and_return(current_user)
     expect(helper.add_people_to_group_conv_list(conversation)).to include user
   end
+
+  context '#load_group_messages_partial_path' do
+    let(:conversation) { create(:group_conversation) }
+    it "returns load_messages partial's path" do
+      create_list(:group_message, 2, conversation_id: conversation.id)
+      expect(helper.load_group_messages_partial_path(conversation)).to eq(
+        'group/conversations/conversation/messages_list/link_to_previous_messages'
+      )
+    end
+
+    it "returns an empty partial's path" do
+      expect(helper.load_group_messages_partial_path(conversation)).to eq(
+        'shared/empty_partial'
+      )
+    end
+  end
+
 end
