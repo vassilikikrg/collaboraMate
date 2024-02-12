42ed69a1756d08b43fcc6da1bf77d65191d99afb
EktorasFokas <ectoras5555@gmail.com>
2024-02-12 10:39:19 +0200

    Add JavaScript to messenger.js to show a different messenger's version on mobile devices
    for #13

--- app/javascript/messenger.js
+++ app/javascript/messenger.js
@@ -1,4 +1,6 @@
-$(document).on('turbolinks:load ajax:complete', function() {
+// $(document).on('turbolinks:load ajax:complete', function() {
+$(document).on('turbo:load ajax:complete', function() {
+
     var messages_visible = $('.conversation .messages-list ul', this)
                                .has('li').length;
     var previous_messages_exist = $('.conversation .messages-list .load-more-messages', this).length;
@@ -7,4 +9,19 @@ $(document).on('turbolinks:load ajax:complete', function() {
         $('.load-more-messages', this)[0].click();
         $('.conversation .messages-list .loading-more-messages').hide();
     }
+
+    // if the messenger is opened on a smaller screen device
+    // show the messenger's version for mobile devices
+    $(".messenger .col-sm-2 ul").on( "click", "a", function() {
+        var col_2_width = $('.messenger .col-sm-2').css('width');
+        var window_width = '' + $('.messenger').width() + 'px';
+        // check if bootstrap columns are stacked (page is opened on a smaller device)
+        if (col_2_width == window_width) {
+            $('body nav').hide();
+            $('.messenger .col-sm-2').hide();
+            $('.messenger .col-sm-10').show();
+            $('body').css('margin', '0 0 0 0');
+            $('.messenger').css('height', '100vh');
+        }
+    });
 });
